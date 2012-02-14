883bc68c584ff9a5df493ef8e51978141890edca
Yohei Okada <yohei.states@akane.waseda.jp>
2012-02-14 15:40:16 -0800

    changed color and layout

--- js/tab.coffee
+++ js/tab.coffee
@@ -1,8 +1,16 @@
 (($) ->
-	$li = $("#navigation li")	
-	$li.find('a').click ->
-		$li.removeClass('selected')
-		$(@).parent('li').addClass('selected')	
-		return false
-	return 
+	$lis = $("#navigation li")	
+	$lis.find('a').click ->
+		#tab
+		$lis.removeClass('selected')
+		$selected = $(@).parent('li')
+		$selected.addClass('selected')	
+
+		#content
+		index = $lis.index($selected)
+		$(".project").addClass('hidden')
+		$('.tab' + index).removeClass('hidden')
+		false
+
+	return
 )(jQuery);
--- js/tab.js
+++ js/tab.js
@@ -1,10 +1,15 @@
 (function() {
   (function($) {
-    var $li;
-    $li = $("#navigation li");
-    $li.find('a').click(function() {
-      $li.removeClass('selected');
-      $(this).parent('li').addClass('selected');
+    var $lis;
+    $lis = $("#navigation li");
+    $lis.find('a').click(function() {
+      var $selected, index;
+      $lis.removeClass('selected');
+      $selected = $(this).parent('li');
+      $selected.addClass('selected');
+      index = $lis.index($selected);
+      $(".project").addClass('hidden');
+      $('.tab' + index).removeClass('hidden');
       return false;
     });
   })(jQuery);
